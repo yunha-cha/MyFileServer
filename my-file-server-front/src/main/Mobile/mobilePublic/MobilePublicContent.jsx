d23ce21aa8869a185785461332d4be56bcbcb749
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2024-12-11 21:06:18 +0900

    Feat: 모바일 공용 클라우드 삭제 기능, 파일 preview 구현

--- my-file-server-front/src/main/Mobile/mobilePublic/MobilePublicContent.jsx
+++ my-file-server-front/src/main/Mobile/mobilePublic/MobilePublicContent.jsx
@@ -2,24 +2,30 @@ import React from 'react';
 import { calcFileSize, formattedDateTime, getFileIconByExtension } from '../../function';
 import { useSelector } from 'react-redux';
 import s from './MobileMainContent.module.css';
+import { FaTrashAlt } from "react-icons/fa";
 
-const MobilePublicContent = ({contentData}) => {
+const MobilePublicContent = ({contentData, setFileDetail}) => {
     const {
-        changedName,
+        // changedName,
         description,
-        download_count,
-        fileCode,
+        // download_count,
+        // fileCode,
         fileFullPath,
-        originalName,
+        // originalName,
         size,
-        uploadedAt
+        uploadedAt,
+        uploadedByUser
     } = contentData;
     //유저 정보 가져오기
     const {data} = useSelector((state)=>state.user);
     return (
-        <div className={s.object}>
+        <div className={s.object} onClick={()=> setFileDetail((prevData) => ({
+            ...prevData, // 이전 상태 복사
+            isOpen: true, // 상태 업데이트
+            file: contentData // 새 데이터 추가
+        }))}>
             <div className={s.sort}>
-                <img src={getFileIconByExtension(fileFullPath)} className={s.fileIcon}/>
+                <img src={getFileIconByExtension(fileFullPath)} className={s.fileIcon} alt='fileIcon'/>
                 <div className={s.titleSeparator}>
                     <div className={s.titleStyle}>
                         {description}
@@ -29,6 +35,8 @@ const MobilePublicContent = ({contentData}) => {
                         <div className={s.sizeStyle}>{calcFileSize(size)}</div>
                     </div>
                 </div>
+                {data.userCode === uploadedByUser.userCode && 
+                <FaTrashAlt size={15} color="#ff2020" style={{alignSelf:'center'}}/>}
             </div>
         </div>
     );
